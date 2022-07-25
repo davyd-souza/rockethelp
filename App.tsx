2f2ea9a9c0af6d0ae8d45e5282090a01d4ae7f6b
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2022-07-25 11:02:52 -0300

    feat: create SignIn Screen and implemented new theme

--- App.tsx
+++ App.tsx
@@ -1,17 +1,27 @@
-import { NativeBaseProvider } from 'native-base';
+// Dependency
+import { NativeBaseProvider, StatusBar } from 'native-base';
 import React from 'react';
 
 // Components 
 import SignIn from './src/screens/SignIn'
+import Loading from './src/components/Loading'
 
 // Styles
 import { THEME } from './src/styles/theme'
-
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <SignIn/>
+      <StatusBar 
+        barStyle='dark-content'
+        backgroundColor='transparent'
+        translucent
+      />
+
+      { fontsLoaded ? <SignIn /> : <Loading /> }
     </NativeBaseProvider>
   );
 }
