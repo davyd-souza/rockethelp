9804fcdbfbb1fcec7eda47a67cdbf2636550b895
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2022-07-26 19:31:07 -0300

    feat: add Details screen

---
+++ src/screens/Details.tsx
@@ -0,0 +1,11 @@
+// COMPONENT
+import { VStack } from 'native-base'
+import { Header } from '../components/Header'
+
+export function Details() {
+    return (
+        <VStack flex={1} bg="white.500">
+            <Header title="solititation" />
+        </VStack>
+    )
+}
